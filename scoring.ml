87afc491b46ee0eb4bcd1441338bf4c4b605544c
Yaya <y.zakowski@gmail.com>
2016-01-25 14:05:52 +0100

    Working scoring for the final

--- scoring.ml
+++ scoring.ml
@@ -1,16 +1,121 @@
+open Batteries 
+
 exception ParseError of string
+
+type data = {
+  nb_R: int;
+  nb_C: int;
+  nb_A: int;
+  nb_L: int;
+  size: int;
+  nb_B: int;
+  nb_T: int;
+  start: int * int;
+  targets: (int * int) array;
+  wind: (int * int) array array array
+}
+
+let read_data file =
+  let f = BatScanf.Scanning.from_file file in
+  Scanf.bscanf f "%d %d %d\n" 
+    (fun r c a ->
+     Scanf.bscanf f "%d %d %d %d\n"
+     (fun l v b t ->
+      Scanf.bscanf f "%d %d\n"
+      (fun x0 y0 ->
+       let targets = Array.init l (fun _ -> 
+		    Scanf.bscanf f "%d %d\n" (fun x y -> (x,y) ))
+       in
+       let wind = Array.init a 
+		  (fun _ -> 
+		   Array.init r (fun _ -> 
+				 Array.init c 
+				 (fun k ->
+				  if k = c - 1 
+				  then Scanf.bscanf f "%d %d\n" (fun x y -> (x,y))
+				  else Scanf.bscanf f "%d %d " (fun x y -> (x,y)))))
+       in
+       let d: data = {
+	 nb_R = r;
+	 nb_C = c;
+	 nb_A = a;
+	 nb_L = l;
+	 size = v;
+	 nb_B = b;
+	 nb_T = t;
+	 start = x0,y0;
+	 targets = targets;
+	 wind = wind
+       }
+       in
+       d
+      )))
+
+let score_turn data loons =
+  let is_covered_by (tx,ty) mb = match mb with
+    | Some (bx,by) ->
+       (bx - tx) * (bx - tx)  +
+	 (min (abs (by - ty)) (data.nb_C - abs (by - ty))) * (min (abs (by - ty)) (data.nb_C - abs (by - ty))) <= data.size * data.size
+    | None -> false
+  in
+  Array.fold_left (fun acc t -> let cov = Array.fold_left (fun acc b -> acc || is_covered_by t b) false loons 
+				in if cov then acc + 1 else acc) 
+		  0 data.targets
+
+let simulate data scenario =
+  let loons = Array.init data.nb_B (fun _ -> Some data.start) in
+  let alt   = Array.init data.nb_B (fun _ -> 0) in
+  scenario |> List.fold_left 
+		(fun acc moves ->
+		 (* Printf.printf "loons beginning of turn: %s at alt %s\n" (BatPervasives.dump loons) (BatPervasives.dump alt); *)
+		 Array.iteri (fun i m -> 
+			    if m = -1 
+			    then 
+			      (if alt.(i) >= 2 then alt.(i) <- alt.(i) - 1 
+			       else raise (ParseError ("Illegal diminution of altitude for balloon " ^ string_of_int i)))
+			    else if m = 1
+ 			    then 
+			      (if alt.(i) < data.nb_A then alt.(i) <- alt.(i) + 1 
+			       else raise (ParseError ("Illegal augmentation of altitude for balloon " ^ string_of_int i)))
+			    else ()) moves; 
+		 (* Printf.printf "loons after order: %s at alt %s\n" (dump loons) (dump alt); *)
+		 Array.iteri (fun i b -> 
+			      (match b with
+			       | Some (bx,by) ->
+				  let a = alt.(i) in
+				  if a = 0 then ()
+				  else let (vx,vy) = data.wind.(a - 1).(bx).(by) in
+				       let bx' = bx + vx in
+				       let by' = (by + vy) mod data.nb_C in
+				       let newb = if bx' >= 0 && bx' < data.nb_R then Some (bx', by') else None 
+				       in loons.(i) <- newb						 
+			       | None -> loons.(i) <- None)			      
+		) loons;
+		 (* Printf.printf "loons after moves: %s at alt %s\n\n" (BatPervasives.dump loons) (BatPervasives.dump alt); *)
+		 acc + score_turn data loons
+		) 0 
 	  
 let scoring s =
+  let data = read_data "final_round.in" in
+  (* Printf.printf "data: %s\n" (dump data); *)
   let lines = Str.split (Str.regexp "\n") s in 
+  (* Printf.printf "Lines: %s\n" (dump lines); *)
   let nb_lines = List.length lines in
-  if nb_lines <> 400 
-  then raise (ParseError ("Your solution contains " ^ string_of_int nb_lines ^ " lines where 400 were expected."))
-  else 
-    lines |> List.iteri 
-	       (fun i line -> 
-		let numbers = Str.split (Str.regexp "[ \t]+") line in
-		let nb_numbers = List.length numbers in
-		if nb_numbers <> 53
-		then raise (ParseError ("The line number " ^ string_of_int i ^ " from your solution contains " ^ string_of_int nb_numbers ^ " numbers where 53 were expected."))
-		else ());
-  0
+  let scenario =
+    if nb_lines <> data.nb_T 
+    then raise (ParseError ("Your solution contains " ^ string_of_int nb_lines ^ " lines where " ^ string_of_int data.nb_T ^ "were expected."))
+    else 
+      begin
+	lines |> List.mapi 
+		   (fun i line -> 
+		    let numbers = Str.split (Str.regexp "[ \t]+") line in
+		    let nb_numbers = List.length numbers in
+		    if nb_numbers <> data.nb_B
+		    then raise (ParseError ("The line number " ^ string_of_int i ^ " from your solution contains " ^ string_of_int nb_numbers ^ " numbers where " ^ string_of_int data.nb_B ^ " were expected."))
+		    else Array.init data.nb_B (fun k -> int_of_string (List.nth numbers k)))	
+      end
+  in
+  (* Printf.printf "Scenario: %s\n" (BatPervasives.dump scenario); *)
+  let res = simulate data scenario in
+  res;;
+
